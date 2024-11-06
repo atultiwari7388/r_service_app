52fcfcff8ad2b01f0a49b70972066e2542b8f067
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-06 13:02:10 +0530

    web app setup

---
+++ rabbit-web-app/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Rabbit-Truck Repair Services",
+  description: "Created by Mylex infotech",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
