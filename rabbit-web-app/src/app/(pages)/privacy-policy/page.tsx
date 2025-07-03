357831c1e71b8bbe5b66c9013dbd83d3bc496d66
Atul Tiwari <tiwariatul9526@gmail.com>
2025-07-03 10:23:55 +0530

    privacy-policy issue fixed

--- rabbit-web-app/src/app/(pages)/privacy-policy/page.tsx
+++ rabbit-web-app/src/app/(pages)/privacy-policy/page.tsx
@@ -2,7 +2,6 @@
 
 import { useAuth } from "@/contexts/AuthContexts";
 import { db } from "@/lib/firebase";
-import { GlobalToastError } from "@/utils/globalErrorToast";
 import { LoadingIndicator } from "@/utils/LoadinIndicator";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -27,7 +26,8 @@ export default function PrivacyPolicy(): JSX.Element {
         return privacyPolicyData;
       }
     } catch (error) {
-      GlobalToastError(error);
+      console.error("Error fetching privacy policy:", error);
+      // GlobalToastError(error);
     } finally {
       setIsLoading(false);
     }
@@ -58,7 +58,26 @@ export default function PrivacyPolicy(): JSX.Element {
                 ) : (
                   <div className="text-gray-700 leading-relaxed space-y-6">
                     <h2 className="text-2xl font-bold text-gray-800 mb-4">
-                      Privacy Policy
+                      Privacy Policy – December, 2024 Introduction. We at Rabbit
+                      Mechanic Service are dedicated to protecting your online
+                      privacy and understand that you need to manage and secure
+                      any personally identifiable information you provide to us.
+                      To help you understand how we plan to handle your personal
+                      information, Rabbit Mechanic Service has created this
+                      online privacy policy. Any information that can be used to
+                      identify a person is considered personal information. This
+                      includes, but is not limited to, a person&apos;s first and
+                      last name, home or other physical address, email address,
+                      or other contact details, whether at work or at home, as
+                      well as any other information about that person. We
+                      reserve the right to make changes to this online privacy
+                      policy at any time or from time to time without giving you
+                      advance notice. You should review any updated online
+                      privacy policy before using this website again. The most
+                      recent version will be posted here (and appropriately
+                      date-stamped). You will be bound by the terms of this
+                      online privacy policy, whether you read it in its original
+                      form or any updates.
                     </h2>
                   </div>
                 )}
