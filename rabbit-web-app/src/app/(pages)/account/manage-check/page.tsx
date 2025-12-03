17c4602bd3fcb5d6b1f600a8c33c15ae3545b757
Atul Tiwari <tiwariatul9526@gmail.com>
2025-12-03 13:24:50 +0530

    padding-top-2 changes done

--- rabbit-web-app/src/app/(pages)/account/manage-check/page.tsx
+++ rabbit-web-app/src/app/(pages)/account/manage-check/page.tsx
@@ -867,7 +867,7 @@ export default function ManageCheckScreen() {
         background: white;
         font-family: "Univers", sans-serif;
         line-height: 1.1;
-        padding-top: 7mm;
+        padding-top: 2mm;
       }
 
       .check-container {
